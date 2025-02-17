7ab885b0cbb0e499cde08249a23ff4b9f937131c
Jesse Khorasanee <jessekhorasanee@gmail.com>
2025-02-17 18:46:01 +0700

    Create a scene of random blocks and add some text:
    Still having an issue with using mapcontrols in typescript, not really
    sure what to try next

--- src/utils/Base3d.ts
+++ src/utils/Base3d.ts
@@ -7,11 +7,14 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
 
 class Base3d {
   public container: HTMLElement
   public camera: PerspectiveCamera
   public scene: Scene
+  public font: any
   public stats
   public clock: Clock
   public renderer: WebGLRenderer
@@ -20,13 +23,15 @@ class Base3d {
 
   constructor(selector: string) {
     this.container = document.querySelector(selector) as HTMLElement
-    this.camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 100)
+    this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+    this.camera.position.set( 0, 200, - 400 );
     this.scene = new THREE.Scene()
     this.stats = new Stats()
     this.clock = new THREE.Clock()
     this.renderer = new THREE.WebGLRenderer({ antialias: true })
     this.controls = new OrbitControls(this.camera, this.renderer.domElement)
     this.mixer = null
+    this.font = null
     this.init()
   }
 
@@ -41,7 +46,7 @@ class Base3d {
     // 控制器
     this.initControls()
     // 添加物体
-    this.setModel()
+    this.creatWorld()
     // 监听场景大小改变，调整渲染尺寸
     window.addEventListener('resize', this.onWindowResize.bind(this))
   }
@@ -52,8 +57,9 @@ class Base3d {
 
   initScene() {
     const pmremGenerator = new THREE.PMREMGenerator(this.renderer)
-    this.scene.background = new THREE.Color(0xbfe3dd)
-    this.scene.environment = pmremGenerator.fromScene(new RoomEnvironment(), 0.04).texture
+    this.scene.background = new THREE.Color( 0xcccccc );
+	  this.scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+
   }
 
   initCamera() {
@@ -64,7 +70,7 @@ class Base3d {
     this.controls = new OrbitControls(this.camera, this.renderer.domElement)
     this.controls.target.set(0, 0.5, 0)
     this.controls.update()
-    this.controls.enablePan = false
+    this.controls.enablePan = true
     this.controls.enableDamping = true
   }
 
@@ -86,29 +92,62 @@ class Base3d {
     this.renderer.render(this.scene, this.camera)
   }
 
-  setModel() {
-    const dracoLoader = new DRACOLoader()
-    dracoLoader.setDecoderPath('./draco/gltf/')
-    const loader = new GLTFLoader()
-    loader.setDRACOLoader(dracoLoader)
-    loader.load(
-      './models/gltf/LittlestTokyo.glb',
-      (gltf) => {
-        const model = gltf.scene
-        model.position.set(1, 1, 0)
-        model.scale.set(0.01, 0.01, 0.01)
-        this.scene.add(model)
+  creatWorld() {
+    const geometry = new THREE.BoxGeometry();
+    geometry.translate( 0, 0.5, 0 );
+    const material = new THREE.MeshPhongMaterial( { color: 0xeeeeee, flatShading: true } );
+    const textMaterial = new THREE.MeshPhongMaterial( { color: 0x8f1856, flatShading: false } );
+    var loader = new FontLoader();
 
-        this.mixer = new THREE.AnimationMixer(model)
-        this.mixer.clipAction(gltf.animations[0]).play()
 
-        console.log('模型添加成功')
-        this.animate()
-      },
-      (e) => {
-        console.log('模型添加失败' + e)
+    loader.load( 'font/helvetiker.typeface.json', (font) =>  {
+      
+
+      for ( let i = 0; i < 500; i ++ ) {
+
+        const mesh = new THREE.Mesh( geometry, material );
+        const posx = Math.random() * 1600 - 800; 
+        const posz = Math.random() * 1600 - 800;
+        mesh.position.x = posx;
+        mesh.position.y = 0;
+        mesh.position.z = posz;
+        mesh.scale.x = 20;
+        mesh.scale.y = Math.random() * 80 + 10;
+        mesh.scale.z = 20;
+        mesh.updateMatrix();
+        mesh.matrixAutoUpdate = false;
+
+        if(i%10 === 0) {
+
+          const text = new TextGeometry('Keep Looking...', {
+            font: font,
+            size: 80,
+            curveSegments: 12,
+            bevelEnabled: true,
+            bevelThickness: 10,
+            bevelSize: 8,
+            bevelOffset: 0,
+            bevelSegments: 5
+          })
+          let textMesh = new THREE.Mesh(text, material)
+          textMesh.position.x = posx;
+          textMesh.position.y = 0;
+          textMesh.position.z = posz;
+          textMesh.scale.x = .2;
+          textMesh.scale.y = .2;
+          textMesh.scale.z = .2;
+          this.scene.add(textMesh)
+
+          const light = new THREE.PointLight( 0xff0000, 1, 100 );
+          light.position.set( posx, 3, posz);
+          this.scene.add( light );
+        }
+        this.scene.add( mesh );
+
       }
-    )
+    })
+
+    this.animate()
   }
 
   onWindowResize() {
